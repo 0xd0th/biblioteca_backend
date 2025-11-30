6e595101ad7345ba321096968cd193fbc96a1adb
d0th <49924099+0xd0th@users.noreply.github.com>
2025-11-30 00:07:57 -0300

    inicializacao

---
+++ src/test/java/com/unifor/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifor.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
